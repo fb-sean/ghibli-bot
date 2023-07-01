dc738ed4529297236764e77c478272271f2b146e
fb-sean <sean@sattler.dev>
2023-07-01 21:40:30 +0200

    Added auto complete

--- src/commands/movie.js
+++ src/commands/movie.js
@@ -1,75 +1,115 @@
-const { Embed } = require("interactions.js");
+const {Embed} = require("interactions.js");
 const axios = require("axios");
 
 module.exports = {
-  name: "movie",
-  description: "Display information about a Ghibli Studio movie",
-  options: [
-    {
-      name: "title",
-      description: "The movie title you want to get information about",
-      type: 3,
-      required: true,
-    },
-  ],
+    name: "movie",
+    description: "Display information about a Ghibli Studio movie",
+    options: [
+        {
+            name: "title",
+            description: "The movie title you want to get information about",
+            type: 3,
+            required: true,
+        },
+    ],
 
-  async execute(interaction, client) {
-    const errorembed = new Embed()
-      .setTitle("Error")
-      .setColor("#FF0000")
-      .setDescription("The movie title you provided is invalid!");
-    let movieEmbed = new Embed();
+    async autoComplete(interaction, client) {
+        const input = interaction.options.getStringOption("title").value ?? '';
 
-    let string = interaction.options.getStringOption("title").value;
-
-    let filmdata;
-    await axios({
-      method: "get",
-      url: `https://ghibli.rest/films?search=${string}`,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .catch((err) => {
-        return interaction
-          .followUp({
-            embeds: [errorembed],
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .then(async (res) => {
-        if (res?.status === 200) {
-          filmdata = res.data[0];
-          movieEmbed
-            .setTitle(filmdata.title)
-            .setDescription(filmdata.description)
-            .setColor("#2B2D31")
-            .addFields([
-              {
-                name: "Original Title",
-                value: `${filmdata.original_title} ||${filmdata.original_title_romanised}||`,
-                inline: false,
-              },
-              { name: "Director", value: filmdata.director, inline: true },
-              { name: "Producer", value: filmdata.producer, inline: true },
-              { name: "Release", value: filmdata.release_date, inline: false },
-              { name: "Runtime", value: filmdata.running_time, inline: true },
-            ])
-
-            .setImage(filmdata.image)
-            .setThumbnail(filmdata.movie_banner);
-
-          return interaction
-            .followUp({
-              embeds: [movieEmbed],
+        axios
+            .get('https://ghibli.rest/films?search=' + input, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                }
             })
-            .catch((err) => {
-              console.log(err);
+            .then(res => {
+                const {
+                    data,
+                    status
+                } = res;
+
+                if (status !== 200) return interaction.sendAutoComplete([
+                    {
+                        name: "Cannot find any movie with that title.",
+                        value: "placeholder",
+                    }
+                ]);
+
+                return interaction.sendAutoComplete(data.map(movie => {
+                    return {
+                        name: movie.title,
+                        value: movie.title,
+                    }
+                }).slice(0, 25));
+            })
+            .catch(error => {
+                console.error(error);
+                return interaction.sendAutoComplete([
+                    {
+                        name: "Cannot find any movie with that title.",
+                        value: "placeholder",
+                    }
+                ]);
             });
-        } 
-      });
-  },
+    },
+    async execute(interaction, client) {
+        const errorembed = new Embed()
+            .setTitle("Error")
+            .setColor("#FF0000")
+            .setDescription("The movie title you provided is invalid!");
+        let movieEmbed = new Embed();
+
+        let string = interaction.options.getStringOption("title").value;
+
+        let filmdata;
+        await axios({
+            method: "get",
+            url: `https://ghibli.rest/films?search=${string}`,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .catch((err) => {
+                return interaction
+                    .followUp({
+                        embeds: [errorembed],
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            })
+            .then(async (res) => {
+                if (res?.status === 200) {
+                    filmdata = res.data[0];
+                    movieEmbed
+                        .setTitle(filmdata.title)
+                        .setDescription(filmdata.description)
+                        .setColor("#2B2D31")
+                        .addFields([
+                            {
+                                name: "Original Title",
+                                value: `${filmdata.original_title} ||${filmdata.original_title_romanised}||`,
+                                inline: false,
+                            },
+                            {name: "Director", value: filmdata.director, inline: true},
+                            {name: "Producer", value: filmdata.producer, inline: true},
+                            {name: "Release", value: filmdata.release_date, inline: false},
+                            {name: "Runtime", value: filmdata.running_time, inline: true},
+                        ])
+
+                        .setImage(filmdata.image)
+                        .setThumbnail(filmdata.movie_banner);
+
+                    return interaction
+                        .followUp({
+                            embeds: [movieEmbed],
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                }
+            });
+    },
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,16 +1,29 @@
 module.exports = async (client, interaction) => {
-    interaction.deferReply()
-    if (!interaction.isCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-      command.execute(interaction, client);
-    } catch (err) {
-      if (err) console.error(err);
-      return interaction.editReply({
-        content: 'An error occurred while executing that command.',
-        ephemeral: true,
-      });
+    if(interaction.isCommand()) {
+        interaction.deferReply()
+
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
+
+        try {
+            await command.execute(interaction, client);
+        } catch (err) {
+            if (err) console.error(err);
+            return interaction.editReply({
+                content: 'An error occurred while executing that command.',
+                ephemeral: true,
+            });
+        }
     }
-  };
-  
+
+    if(interaction.isAutoComplete()) {
+        const command = client.commands.get(interaction.commandName);
+        if (!command && command.autoComplete) return;
+
+        try {
+            await command.autoComplete(interaction, client);
+        } catch (err) {
+            if (err) console.error(err);
+        }
+    }
+};
